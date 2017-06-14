29b2ef282f1d440e3a796e4cff6336aae39fe951
odinlake <mikael@odinlake.net>
2017-06-14 23:19:10 +0100

    add theme cube to picture links

--- webui/pictures.php
+++ webui/pictures.php
@@ -49,10 +49,18 @@
 	              	<H3 class="px-3">Data Exploration and Visualization</H3>
 	            	<TABLE class="table table-striped">
 		            	<tbody>
-			            	<tr>
-				            	<td><A href = ":3838/shiny/">Story Simularity</A></td>
-				            	<td>Pick a story and see other stories that are thematically similar.</td>
-				            </tr>
+                            <tr>
+                                <td><A href = ":3838/shiny/">Story Simularity</A></td>
+                                <td>Pick a story and see other stories that are thematically similar.</td>
+                            </tr>
+                            <tr>
+                                <td><A href = "svg.php?subject=themecube">Star Trek tos/tas/tng Theme Domain Distribution</A></td>
+                                <td>
+                                    A case study of how themes are distributed between the three early Star Trek TV Series. 
+                                    This delineates, to an extent, how the show changed over the years.
+                                </td>
+                            </tr>
+                        </tbody>
 	            	</TABLE>
             	</div>
             </div>
