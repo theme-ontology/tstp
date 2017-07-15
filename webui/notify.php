a4358a87cb6b5b40326e58ec69d4372a10595612
odinlake <mikael@odinlake.net>
2017-07-15 17:56:59 +0100

    append git hook log

--- webui/notify.php
+++ webui/notify.php
@@ -4,7 +4,7 @@ if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push')
 {
     file_put_contents("/var/tmp/tstpnotify.payload", $_POST['payload']);
     $res = shell_exec('sudo -u ec2-user /home/ec2-user/dev/tstp/scripts/run stagethemes.sh 2>&1');
-    file_put_contents("/var/tmp/tstpnotify.result", $res);
+    file_put_contents("/var/tmp/tstpnotify.result", $res, FILE_APPEND);
     echo "done";
 }
 echo "nothing to do";
