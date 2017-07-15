c0ce52b9046ff0368acc830e22ec8eb610c586df
odinlake <mikael@odinlake.net>
2017-07-15 13:38:07 +0100

    updated hook response

--- webui/notify.php
+++ webui/notify.php
@@ -4,6 +4,8 @@ if ( $_POST['payload'] )
 {
     file_put_contents("/var/tmp/tstpnotify.payload", $_POST['payload']);
     shell_exec('sudo -u ec2-user /home/ec2-user/dev/tstp/scripts/run stagethemes.sh');
+    echo "done";
 }
+echo "nothing to do";
 
 ?>
