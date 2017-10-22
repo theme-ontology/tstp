e876ee6f647f4faf76c51d34418060437896851a
odinlake <mikael@odinlake.net>
2017-10-22 15:16:33 +0100

    fix center column formatting (removes horizontal scrollbar that appeared after last commit)

--- webui/index.php
+++ webui/index.php
@@ -4,30 +4,25 @@
 <meta charset="UTF-8">
 <title>TSTP</title>
 <?php include "header.php"; ?>
-
 </head>
 
 <body>
-<div class="row">
-    <div class="col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2">
-        <div style="width:70em;">
-            <P class="text-center center-block">
-        		<img style="max-width: 800px;" src="img/metatheme-triangle.svg"/>
-        	</P>
-            <div style="padding:2em 10em;">
-                <P>
-                    <A href="http://www.themeontology.org">themeontology.org</A> is a database of stories and well defined, well organized themes used in the stories.
-                    The data is defined in 
-                    <A href="https://github.com/odinlake/theming">our git repository</A>.
-                    The picture above is a way of visualizing how the types of themes differ between three subsets of the stories,
-                    namely the first three Star Trek series.
-                </P>
-                <P>
-                    Get in touch with Mikael Onsjö and Paul Sheridan at 
-                    <font color="#337ab7">&#105;&#110;&#102;&#111;&#64;&#116;&#104;&#101;&#115;&#116;&#97;&#114;&#116;&#114;&#101;&#107;&#112;&#114;&#111;&#106;&#101;&#99;&#116;&#46;&#110;&#101;&#116;</font>
-                    for more information.
-                </P>
-            </div>
+<div class="container main-body" style="text-align:center;">
+    <div style="max-width:60em; display:inline-block;">
+    	<img src="img/metatheme-triangle.svg"/>
+        <div style="padding:2em; text-align:left;">
+            <P>
+                <A href="http://www.themeontology.org">themeontology.org</A> is a database of stories and well defined, well organized themes used in the stories.
+                The data is defined in 
+                <A href="https://github.com/odinlake/theming">our git repository</A>.
+                The picture above is a way of visualizing how the types of themes differ between three subsets of the stories,
+                namely the first three Star Trek series.
+            </P>
+            <P>
+                Get in touch with Mikael Onsjö and Paul Sheridan at 
+                <font color="#337ab7">&#105;&#110;&#102;&#111;&#64;&#116;&#104;&#101;&#115;&#116;&#97;&#114;&#116;&#114;&#101;&#107;&#112;&#114;&#111;&#106;&#101;&#99;&#116;&#46;&#110;&#101;&#116;</font>
+                for more information.
+            </P>
         </div>
     </div>
 </div>
