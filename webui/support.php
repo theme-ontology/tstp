3202713272dae30763c810024f6c31f68fd8c473
odinlake <mikael@odinlake.net>
2018-03-05 23:31:36 +0000

    pretty up support page

--- webui/support.php
+++ webui/support.php
@@ -19,9 +19,17 @@
                 If you would like to help us with this you can use one of the methods below to make a donation.
             </P>
 
-            <H3>PayPal</H3>
+            <P>
+                In any event, don't hesitate to contact us:
+                <font color="#337ab7">&#x63;&#x6F;&#x6E;&#x74;&#x61;&#x63;&#x74;&#x40;&#x74;&#x68;&#x65;&#x6D;&#x65;&#x6F;&#x6E;&#x74;&#x6F;&#x6C;&#x6F;&#x67;&#x79;&#x2E;&#x6F;&#x72;&#x67;</font>.
+            </P>
 
-            <TABLE class="table borderless vcenter">
+
+            <H3 class="space-large">PayPal</H3>
+
+            <P>Click the button below and follow instructions in order to donate using PayPal:</P>
+
+            <TABLE class="table borderless vcenter text-center">
                 <tr>
                     <td>
                         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
@@ -32,54 +40,45 @@
                     </form>
                     </td>
                     <td>
-                        <P>Click the button to the left and follow instructions in order to donate using PayPal.</P?
                         <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
                     </td>
                 </tr>
             <TABLE>
 
-            <div>
-            </div>
 
-            <H3>Crypto Currencies</H3>
+            <H3 class="space-large">Crypto Currencies</H3>
 
-            <TABLE class="table borderless vcenter">
+            <P>Donate crypto by sending to one of our addresses below:</P>
+
+            <TABLE class="table borderless vcenter shrink-to-fit">
                 <tr>
                     <th></th>
                     <th>Currency</th>
                     <th>Address</th>
                 </tr>
                 <tr>
-                    <td><img src="img/crypto_BCH_medium.png"></td>
-                    <td>Bitcoin Cash (BCH)</td>
+                    <td class="text-center"><img src="img/crypto_BCH_medium.png"></td>
+                    <td>BCH</td>
                     <td>
                         <code>1sKmYJcpH7FGoxz42Xs1BHFbZht4LumyU</code>
                     </td>
                 </tr>
                 <tr>
-                    <td><img src="img/crypto_ETH_medium.png"></td>
-                    <td>Ethereum (ETH)</td>
-                    <td>
-                        <code>0x22F252375aE2e1bbc7C1e4387c8Dd4a9310C34df</code>
-                    </td>
-                </tr>
-                <tr>
-                    <td><img src="img/crypto_BTC_medium.png"></td>
-                    <td>Bitcoin (XBT)</td>
+                    <td class="text-center"><img src="img/crypto_BTC_medium.png"></td>
+                    <td>XBT</td>
                     <td>
                         <code>bc1quyu459quaz6p99jsy39mrja7sgqwptn2zhgutc</code>
                     </td>
                 </tr>
+                <tr>
+                    <td class="text-center"><img src="img/crypto_ETH_medium.png"></td>
+                    <td>ETH</td>
+                    <td>
+                        <code>0x22F252375aE2e1bbc7C1e4387c8Dd4a9310C34df</code>
+                    </td>
+                </tr>
             </TABLE>
 
-            <P>
-                Ethereum and Bitcoin Cash have the lowest transaction fees and are thus recommended. 
-            </P>
-
-            <P>
-                In any event, don't hesitate to contact us:
-                <font color="#337ab7">&#x63;&#x6F;&#x6E;&#x74;&#x61;&#x63;&#x74;&#x40;&#x74;&#x68;&#x65;&#x6D;&#x65;&#x6F;&#x6E;&#x74;&#x6F;&#x6C;&#x6F;&#x67;&#x79;&#x2E;&#x6F;&#x72;&#x67;</font>.
-            </P>
 
         </div>
     </div>
