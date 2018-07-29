2a6280989e10b6177d1429d72535476528934441
odinlake <mikael@odinlake.net>
2018-07-29 17:30:47 +0100

    try to index using Solr

--- pylib/util/task_cache_webquery.py
+++ pylib/util/task_cache_webquery.py
@@ -1,5 +1,7 @@
 import log
 import webquerylib
+import subprocess
+import os
 
 
 def main():
@@ -7,4 +9,10 @@ def main():
     webquerylib.cache_special_queries()
     log.debug("START cache_objects")
     webquerylib.cache_objects()
+
+    if os.name != 'nt' and os.path.isdir('/usr/local/solr'):
+        log.debug("INDEX using Solr")
+        subprocess.call("/usr/local/solr/bin/post -c tstp /tmp/webjson/storydefinitions/*.json", shell=True)
+        subprocess.call("/usr/local/solr/bin/post -c tstp /tmp/webjson/themedefinitions/*.json", shell=True)
+
 
