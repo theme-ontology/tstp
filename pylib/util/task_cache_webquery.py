eede9dcd36f85ae8542d0a348dee52bc1463d4d6
Mikael/ec2-user <mikael@odinlake.net>
2018-07-29 17:49:22 +0000

    clear solr index before populating it

--- pylib/util/task_cache_webquery.py
+++ pylib/util/task_cache_webquery.py
@@ -12,7 +12,8 @@ def main():
 
     if os.name != 'nt' and os.path.isdir('/usr/local/solr'):
         log.debug("INDEX using Solr")
+        delcmd = "curl http://localhost:8983/solr/tstp/update?commit=true -H \"Content-Type: text/xml\" --data-binary '<delete><query>*:*</query></delete>'"
+        subprocess.call(delcmd, shell=True)
         subprocess.call("/usr/local/solr/bin/post -c tstp /tmp/webjson/storydefinitions/*.json", shell=True)
         subprocess.call("/usr/local/solr/bin/post -c tstp /tmp/webjson/themedefinitions/*.json", shell=True)
-
 
