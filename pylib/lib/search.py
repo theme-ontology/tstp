4b1c7b7b0a8d6d917d8d09a308f68cb2518cee7c
odinlake <mikael@odinlake.net>
2018-08-26 18:00:24 +0100

    improve search with completions

--- pylib/lib/search.py
+++ pylib/lib/search.py
@@ -34,6 +34,13 @@ def query(core, qtype, q):
             "pf": " ".join("%s^%s" % (k, v) for k, v in  FIELD_WEIGHT.iteritems()),
             "rows": 1000,
         })
+    elif qtype == "suggest":
+        params.update({
+            "suggest.dictionary": "completer",
+            "rows": 10,
+        })
+    elif qtype == "spell":
+        pass
 
     url = URL_BASE + core + "/" + qtype + "?" + urllib.urlencode(params)
     with lib.log.Timer(url if DEBUG else None):
@@ -52,14 +59,19 @@ def find(core, q):
     replacements = []
     scores = defaultdict(int)
 
+    q = q.strip()
     qlist = [ unicode(w.strip()) for w in q.split(" ") if w.strip() ]
+    qset = set(kw.lower() for kw in qlist)
     if not qlist: return []
+    if DEBUG: lib.log.debug("query list: %s", qlist)
+
+    # find spelling variations
     spellresult = query(core, "spell", q)
     variations = spellresult['spellcheck']['suggestions']
     bykw = {kw:set() for ii, kw in enumerate(variations) if ii%2==0}
-
     for ii, kw in enumerate(variations):
-        if ii%2 == 0:
+        # ignores spelling suggestions for words with asterisks and gunk
+        if ii%2 == 0 and kw.isalpha():
             for spec in variations[ii+1]['suggestion']:
                 word = spec['word']
                 weight = spec['freq']
@@ -72,7 +84,35 @@ def find(core, q):
     for kw, word in weights.keys():
         weights[(kw, word)] /= float(max_weights[kw])
 
+    # find suggestions for completing the last word if there's no gunk in it
+    kw = qlist[-1].lower()
+    if kw.isalpha():
+        maxweight = -1
+        completeresults = query(core, "suggest", q)
+        try:
+            completions = completeresults["suggest"]["completer"][q]['suggestions']
+        except KeyError:
+            completions = []
+            if DEBUG:
+                lib.log.error("unexpected result from completer...")
+
+        for ii, cspec in enumerate(completions):
+            word = cspec['term'].split('\x1e')[-1]
+            weight = cspec['weight']
+            maxweight = max(maxweight, weight)
+            if weight < maxweight / 10.0 or ii > 5: break
+            spec = { 'word': word, 'freq': weight }
+            order.append(spec)
+            if kw not in bykw:
+                bykw[kw] = set()
+            bykw[kw].add(word)
+            weights[(kw, word)] = weight / float(maxweight)
+
     order.sort(key=lambda spec: -spec['freq'])
+
+    if DEBUG:
+        for kw in sorted(bykw):
+            lib.log.debug("trying variations for %s: %s", kw, sorted(bykw[kw]))
 
     while True:
         count = prod(len(x)+1 for x in bykw.itervalues())
@@ -98,7 +138,7 @@ def find(core, q):
                 basescore += weights[(kw, word)] * 0.5
         basescore /= float(len(replace_list)) + 1
         rlu = {kw.lower(): word for kw, word in replace_list}
-        nq = " ".join(rlu.get(w.lower(), w) for w in qlist)
+        nq = " ".join(rlu.get(w.lower().strip("*"), w) for w in qlist)
         result = query(core, "select", nq.encode('utf8'))['response']['docs']
         for spec in result:
             score = (basescore * spec['score']) / 10.0
