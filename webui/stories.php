f25f8a44ee14eb24ab6baf4e6dac912578b25454
odinlake <mikael@odinlake.net>
2018-10-17 23:21:23 +0100

    fix typo

--- webui/stories.php
+++ webui/stories.php
@@ -156,7 +156,6 @@
 
         <div id="div_stories_datatable" class="col-md-12 hpad0">
         	<div class="basebox">
-                fjdhsabkjh
 	            <table id="stories_datatable" class="display table table-striped" cellspacing="0" width="100%">
 			        <thead>
 			            <tr>
