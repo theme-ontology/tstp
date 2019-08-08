83309d21a70c341c28e66203acbec7e5b2acd258
odinlake <mikael@odinlake.net>
2019-08-08 11:23:40 +0100

    modernize credentials template

--- pylib/credentials_template.py
+++ pylib/credentials_template.py
@@ -2,25 +2,22 @@
 #: this file should be copied to credentials.py and edited
 
 DBS = {
-	'tstp@dreamhost' : dict(
-		host = 'sql.thestartrekproject.net',
-		db = 'thestartrekproject_master',
-		user = '',
-		password = '',
-	),
-	
 	'tstp@aws' : dict(
 		host = '127.0.0.1',
 		db = 'theme_ontology_web',
 		user = '',
 		password = '',
 	),
-	
+	'tstp@ldnhome' : dict(
+		host = '127.0.0.1',
+		db = 'theme_ontology_web',
+		user = '',
+		password = '',
+	),
 }
 
-#: "tstp" will be used by the web 
-DBS['tstp'] = DBS['tstp@dreamhost']
-
+#: "tstp" will be used by the web scripts
+DBS['tstp'] = DBS['tstp@aws']
 
 #: the url used to test this deployment, must end with /
 SERVER_URL = "http://127.0.0.1/tstp/webui/"
