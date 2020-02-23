9ca0877f5a9af0a55c9584ad315770058ee7e72c
odinlake <mikael@odinlake.net>
2020-02-23 15:38:15 +0000

    remove old stuff from footer

--- webui/footer.php
+++ webui/footer.php
@@ -7,16 +7,15 @@
 			//var_dump($_SERVER);
 			?></span>
 
-			 <A href="upload">workflow</A>
-			 | <A href="util">util</A>
+			 <A href="util">util</A>
 			 | <A href="log">log</A>
 			 | <A href="m4">m4</A>
 
-<?php if ($_SERVER["SERVER_PORT"] == "8080") { ?>
+<?php /* if ($_SERVER["SERVER_PORT"] == "8080") { ?>
 			 | <A href="http://www.themeontology.org/">prod</A>
 <?php } else { ?>
 			 | <A href="http://www.themeontology.org:8080/tstp/webui/">uat</A>
-<?php } ?>
+<?php }  */?>
 			 | <A href="support">donate</A>
 
 		</p>
