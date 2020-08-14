7cc3ddfcc59a5bb37e235cc74fa8a8aa6c59c6c0
paul-sheridan <paul.sheridan.stats@gmail.com>
2020-08-14 20:10:14 +0900

    updates on parsing themeobj attributes

--- pylib/webtask/cache_data.py
+++ pylib/webtask/cache_data.py
@@ -4,28 +4,109 @@ from credentials import GIT_THEMING_PATH_HIST
 import lib.commits
 import lib.files
 import lib.dataparse
+import webobject
 from git import Repo
-
+import json
+from collections import OrderedDict
+import datetime
+from pytz import timezone
+import dateutil.parser
 
 def main():
     #' preliminaries
     basepath = GIT_THEMING_PATH_HIST
-    notespath = os.path.join(basepath, "notes")
     os.chdir(basepath)
 
-    #' checkout git repo at a given commid id or version tag
-    #commit_id = '56b4640d27b6f78478cd6aada1189add3480bf17'
-    version_tag = 'v0.3.2'
+    #' checkout v0.3.2 of the repo
     repo = Repo(GIT_THEMING_PATH_HIST)
-    #repo.git.checkout(commit_id)
+    version_tags = repo.tags
+    # [print(tag) for tag in repo.tags]
+    version_tag = version_tags[-1]
     repo.git.checkout(version_tag)
+    commit_id = repo.head.object.hexsha
+    timestamp = repo.head.object.committed_datetime
 
-    #' read theme files
-    deprecated_themes_file = 'deprecated-themes.txt'
+    #' initialize dict object for v0.3.2 of lto
+    lto_od = OrderedDict()
+    metadata_od = OrderedDict()
+    metadata_od['version'] = str(version_tag)
+    metadata_od['timestamp'] = str(timestamp)
+    metadata_od['git-commit-id'] = str(commit_id)
+    lto_od['lto'] = metadata_od
+    lto_od['themes'] = OrderedDict()
+    print(json.dumps(lto_od, indent=4))
+    #themes_list = []
+    #test_theme_dict = { 'name': 'AI assistant', 'description': 'There is an artificial intelligence that acts as a personal assistant to a real person.' }
+    #print(themes_list)
+    #themes_list.append(test_theme_dict)
+    #print(themes_list)
+    #themes_list.append(test_theme_dict)
+    #print(themes_list)
+
+    # ' read theme files
+    #themeobjs = []
+    #for path in lib.files.walk(basepath, ".*\.th\.txt$"):
+    #    if path.endswith(".th.txt"):
+    #        themeobjs.extend(lib.dataparse.read_themes_from_txt(path, addextras=True, combinedescription=False))
+
     theme_files = list(lib.files.walk(basepath, ".*\.th\.txt$"))
     print("\n".join(theme_files))
-    #print(theme_files)
+    themeobjs = list(lib.dataparse.read_themes_from_txt(theme_files[0], addextras=True, combinedescription=False))
+    fields = sorted(set(themeobjs[10].fields + themeobjs[10].extra_fields))
+    #print(fields)
+    json_str = webobject.Theme.make_json(themeobjs, fields=fields)
+    themes = json.loads(json_str)['data']
+    #print(themes[10][1])
+    #print("THEME NO. 11: \n%s" % json.dumps(themes[10], indent=4))
+    #print(type(fields[0]))
+    #theme_dict = { 'name': {}, 'description': {}, 'parents': {} }
 
-    themes = list(lib.dataparse.read_themes_from_txt(theme_files[0], False))
-    print(themes[0])
+    #id = 388
+    id = 379
+    fields = sorted(set(themeobjs[id].fields + themeobjs[id].extra_fields))
+    print(fields)
+    theme_od = OrderedDict()
+    theme_od['name'] = {}
+    theme_od['description'] = {}
+    theme_od['parents'] = {}
+    #print(theme_od)
 
+    print(type(themeobjs[id].name))
+    print(type(themeobjs[id].examples[0].split('\n\n')))
+
+
+    for field in fields:
+        if field == 'name':
+            theme_od['name'] = themeobjs[id].name
+        if field == 'description':
+            theme_od['description'] = themeobjs[id].description.rstrip().replace(' \n','').replace('\n ','').replace('\n','')
+        if field == 'parents':
+            parents = themeobjs[id].parents.split(', ')
+            if len(parents) == 1:
+                theme_od['parents'] = ''.join(parents)
+            elif len(parents) > 1:
+                theme_od['parents'] = parents
+        if field == 'notes':
+            theme_od['notes'] = themeobjs[id].notes.rstrip().replace(' \n','').replace('\n ','').replace('\n','')
+        if field == 'aliases':
+            theme_od['aliases'] = themeobjs[id].aliases
+        if field == 'references':
+            references = filter(None, themeobjs[id].references)
+            print(references)
+            if len(references) == 1:
+                theme_od['references'] = ''.join(references)
+            elif len(references) > 1:
+                theme_od['references'] = references
+        if field == 'examples':
+            raw_examples = themeobjs[id].examples[0].split('\n\n')
+            examples = list()
+            if raw_examples[0] != "":
+                for example in raw_examples:
+                    examples.append(example.rstrip().replace(' \n','').replace('\n ','').replace('\n',''))
+                theme_od['examples'] = examples
+
+    print(json.dumps(theme_od, indent=4))
+
+
+
+main()
