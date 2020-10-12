007bd1dd37149984182729b505e232a60b9ffb73
odinlake <mikael@odinlake.net>
2020-10-12 09:32:48 +0100

    Modern Family episode importer

--- pylib/util/wikiparse_modernfamily.py
+++ pylib/util/wikiparse_modernfamily.py
@@ -5,22 +5,19 @@ import sys
 
 def main():
     fn = sys.argv[-1]
-
     urls = [
-        "https://en.wikipedia.org/wiki/List_of_The_Twilight_Zone_(1985_TV_series)_episodes",
+        "https://en.wikipedia.org/wiki/List_of_Modern_Family_episodes",
     ]
     stories = {}
 
     for idx, url in enumerate(urls):
-        # cols:
-        # titlefield, directorfield, authorfield, datefield
         for story in find_episodes_st1(
-            url, 1, "tz1985e", cols = (1, 2, 3, 4), isterse = False, tableclass = "wikiepisodetable",
+            url, 1, "modfam", tableclass="wikiepisodetable", cols=(1, 2, 3, 4), isterse=True
         ):
             stories[story.name] = story
 
-    objs = [ stories[sid] for sid in sorted(stories) ]
-    txt = webdb.get_defenitions_text_for_objects(objs, empty_storythemes_headers = True, skip_fields = ('collections',), add_fields=("Ratings",))
+    objs = [stories[sid] for sid in sorted(stories)]
+    txt = webdb.get_defenitions_text_for_objects(objs, empty_storythemes_headers=True, skip_fields=('collections',), add_fields=("Ratings",))
     txt = txt.encode("utf-8")
 
     if fn.endswith(".txt"):
