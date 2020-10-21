fd793c28648a7f8f493d19d45d0d9b7ad8b13f63
paul-sheridan <paul.sheridan.stats@gmail.com>
2020-10-21 14:23:36 +0900

    minor edit

--- pylib/tests/lib/textformat.py
+++ pylib/tests/lib/textformat.py
@@ -8,8 +8,9 @@ import unittest
 
 class TestTextFormat(unittest.TestCase):
     def test_remove_word_wrap_column_width_newlines(self):
-        #' check each single newline character is replaced by a space, while any two consecutive
-        #' newline characters are left unmolested
+        #' check each single newline character in string is replaced by a space, while any two
+        #' consecutive newline characters are left unmolested; this does not apply to newline
+        #' characters occurring at the end of the string as they are all stripped
         input_text = "The sea\nis high\n\nagain today,\n\nwith a thrilling\nflush of wind.\n"
         expected_text = "The sea is high\n\nagain today,\n\nwith a thrilling flush of wind."
         output_text = lib.textformat.remove_word_wrap_column_width_newlines(input_text)
