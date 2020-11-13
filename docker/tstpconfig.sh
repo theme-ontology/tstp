f65e8998fcf057e5e3357528067a284a2c4e0d5a
odinlake <mikael@odinlake.net>
2020-11-13 23:29:22 +0000

    further docker fiddling

--- docker/tstpconfig.sh
+++ docker/tstpconfig.sh
@@ -6,11 +6,6 @@ export TSTPDOCKER=1
 if [ -d "$LOCALTSTP" ]; then
     echo "configuring for: UAT"
     ln -s $LOCALTSTP /code/tstp
-    #cp -r /local/tstp /code/tstp
-    #chown -R root:www-data /code/tstp
-    #chmod -R 664 /code/tstp
-    #chmod -R 774 /code/tstp/scripts
-    #find /code/tstp -type d -exec chmod 775 {} \;
 else
     echo "configuring for: PROD"
     echo "..."
