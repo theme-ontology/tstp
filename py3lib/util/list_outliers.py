1805200851db52ffce34ca36f497600f0c6931c4
odinlake <mikael@odinlake.net>
2021-06-20 23:09:23 +0100

    amend last script

--- py3lib/util/list_outliers.py
+++ py3lib/util/list_outliers.py
@@ -1,10 +1,9 @@
 # Copyright 2021, themeontology.org
 # Tests:
 """
-Read one or more text files and write their contents on stdout, while doing TO
-specific cleaning and re-ordering.
-
-Presently works only on story (.st.txt) files.
+Read the ontology from git checkout and report on some things that would seem like
+red flags for one reason or another. For example, report here on stories that have
+an excessive number of choice themes.
 """
 import os.path
 import credentials
@@ -22,4 +21,3 @@ def main():
         nchoice = len(list(story.get("Choice Themes").iter_parts()))
         if nchoice > 5:
             lib.log.warn("[NChoice] Story '%s' has %s choice themes.", story.name, nchoice)
-    to.write_clean()
