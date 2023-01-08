26d629dc1795a98d88875f8b0fdafd7514563d74
odinlake <mikael@odinlake.net>
2023-01-08 21:18:18 +0000

    rearrange

--- docker/totolo/m4/startup.sh
+++ docker/totolo/m4/startup.sh
@@ -2,7 +2,8 @@
 
 URLGIT_TSTP_MASTER="https://github.com/theme-ontology/tstp/archive/refs/heads/master.tar.gz"
 URLGIT_THEMES_MASTER="https://github.com/theme-ontology/theming/archive/refs/heads/master.tar.gz"
-PATH_LOCALAPP="/local/app"
+PATH_LOCALAPP="/local/tstp/totolo/app"
+PATH_GITAPP="/code/tstp/totolo/app"
 
 mkdir /code/tmp
 mkdir /code/tstp
@@ -14,12 +15,16 @@ curl -L $URLGIT_THEMES_MASTER > /code/tmp/theming.tar.gz
 tar -zxf /code/tmp/tstp.tar.gz tstp-master --strip-components 1 -C /code/tstp
 tar -zxf /code/tmp/theming.tar.gz theming-master --strip-components 1 -C /code/theming
 
-#tree -L 3 /code
+tree -L 2 /local/tstp
 
 if [ -d "$PATH_LOCALAPP" ]; then
     cd $PATH_LOCALAPP
-    pwd
+else
+    cd $PATH_GITAPP
 fi
+echo ":: STARTING UP AT PATH ::::::::::::::::::::::::::::::::::"
+pwd
+echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
 
 /code/tstp/scripts/runpy python3 manage.py runserver 0.0.0.0:8000
 
