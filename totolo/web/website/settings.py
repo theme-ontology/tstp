5c1290283d66e8f0f7a8e37643622e0005b599cb
odinlake <mikael@odinlake.net>
2023-01-26 12:58:01 +0000

    totolo support https on aws, maybe?

--- totolo/web/website/settings.py
+++ totolo/web/website/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'totolo.middleware.CloudMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
