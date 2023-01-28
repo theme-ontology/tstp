99b2c61f641323621ff626ed186df353c4ae399e
odinlake <mikael@odinlake.net>
2023-01-28 12:00:36 +0000

    nginx conf update

--- totolo/docker/aws-ecr-push.bat
+++ totolo/docker/aws-ecr-push.bat
@@ -1,4 +1,4 @@
-
+docker context use default
 docker compose -f docker-compose.yaml -f docker-compose.prod.yaml build
 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/m8o2v6v7
--- totolo/docker/aws-ecs-deploy.bat
+++ totolo/docker/aws-ecs-deploy.bat
@@ -1,3 +1,5 @@
+aws-ecr-push.bat
+
 docker context use totolo
-docker compose -f docker-compose.yaml up
+docker compose -f docker-compose.yaml -f docker-compose.prod.yaml up
 docker context use default
