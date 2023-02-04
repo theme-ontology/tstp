9fa332e05f8365d836745d771f173e04afffce71
odinlake <mikael@odinlake.net>
2023-02-04 16:14:32 +0000

    tan-ru fixes

--- totolo/web/totolo/deployment.py
+++ totolo/web/totolo/deployment.py
@@ -1,20 +1,24 @@
 # Copyright 2023, themeontology.org
 # Tests:
 import tempfile
+import os
 
 
 TEMP_PATH = tempfile.gettempdir()
+IS_MONOLITH = os.getenv("IS_MONOLITH", "1") == "1"
 
 
 class SPHINX:
-    HOST = 'sphinx'
+    HOST = 'localhost' if IS_MONOLITH else 'sphinx'
     PORT = 9306
 
 
 class SQLDB:
-    HOST = 'sqldb'
+    HOST = 'localhost' if IS_MONOLITH else 'sqldb'
     PORT = 5432
     db = 'totolo_db'
     user = 'totolo'
     secret = 'totolo'
 
+
+print("::: {} ::: {} ::: {} :::".format(IS_MONOLITH, SPHINX.HOST, SQLDB.HOST))
--- totolo/web/website/settings.py
+++ totolo/web/website/settings.py
@@ -11,27 +11,22 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from pathlib import Path
 import totolo.deployment
+import os
+from django.core.management.utils import get_random_secret_key
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ba(s0f0b_lr@#^%nsfxw9kx3o1r8c$&_&5d(e!60ec3g!tu9=9'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
+DEBUG = True if os.getenv("DEBUG", False) else False
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+if DEBUG:
+    SECRET_KEY = 'django-insecure-ba(s0f0b_lr@#^%nsfxw9kx3o1r8c$&_&5d(e!60ec3g!tu9=9'
+else:
+    SECRET_KEY = get_random_secret_key()
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'ontologyexplorer.apps.OntologyexplorerConfig',
     'rest_framework',
