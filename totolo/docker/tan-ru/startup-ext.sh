758bff7f2aaad808540170ae5fec8ccbf51ca0f7
odinlake <mikael@odinlake.net>
2023-02-12 16:40:35 +0000

    certificate for www

--- totolo/docker/tan-ru/startup-ext.sh
+++ totolo/docker/tan-ru/startup-ext.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 SSL_PRIVKEY_V2_LOC=/etc/ssl/certs/v2.themeontology.org/privkey1.pem
+SSL_PRIVKEY_WWW_LOC=/etc/ssl/certs/www.themeontology.org/privkey1.pem
 
 mkdir -p /etc/ssl/certs/v2.themeontology.org
+mkdir -p /etc/ssl/certs/www.themeontology.org
 
 if [ -n "$SSL_PRIVKEY_V2" ]; then
     echo "$SSL_PRIVKEY_V2" >> $SSL_PRIVKEY_V2_LOC
@@ -10,3 +12,6 @@ else
     cp -r /code/tstp/totolo/cert/localhost /etc/ssl/certs/
 fi
 
+if [ -n "$SSL_PRIVKEY_WWW" ]; then
+    echo "$SSL_PRIVKEY_WWW" >> $SSL_PRIVKEY_WWW_LOC
+fi
