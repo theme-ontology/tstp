bcbbaa294733ef0c909236edfe7e90d497558129
odinlake <mikael@odinlake.net>
2023-08-12 14:53:56 +0100

    remove dead code

--- totolo/docker/tan-ru/startup-ext.sh
+++ totolo/docker/tan-ru/startup-ext.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-#SSL_PRIVKEY_V2_LOC=/etc/ssl/certs/v2.themeontology.org/privkey1.pem
-#SSL_PRIVKEY_WWW_LOC=/etc/ssl/certs/www.themeontology.org/privkey1.pem
-#
-#mkdir -p /etc/ssl/certs/v2.themeontology.org
-#mkdir -p /etc/ssl/certs/www.themeontology.org
-#
-#if [ -n "$SSL_PRIVKEY_V2" ]; then
-#    echo "$SSL_PRIVKEY_V2" >> $SSL_PRIVKEY_V2_LOC
-#else
-#    cp -r /code/tstp/totolo/cert/localhost /etc/ssl/certs/
-#fi
-#
-#if [ -n "$SSL_PRIVKEY_WWW" ]; then
-#    echo "$SSL_PRIVKEY_WWW" >> $SSL_PRIVKEY_WWW_LOC
-#fi
-
-
 url="themeontology.org"
 email="mikael@odinlake.net"
 mkdir -p /opt/letsencrypt
